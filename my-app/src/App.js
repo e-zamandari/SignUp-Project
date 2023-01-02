fc1bf0660ea5289bd4a5a9f2f57a02609d58d4ff
Erfan Zamandari <erfanzamandar9@gmail.com>
2023-01-03 01:02:01 +0330

    SignUp Project

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './Components/Login';
+import {Routes , Route , Navigate} from 'react-router-dom';
+import SignUp from './Components/SignUp';
+function App() {
+  return (
+    <div className="App">
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/' element={<Navigate to="/signup" />} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
